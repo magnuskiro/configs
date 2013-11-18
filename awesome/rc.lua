e465def2d97e35813d89057be18e67e67d62dafd
Magnus Kirø <magnuskiro@gmail.com>
2013-11-18 13:16:58 +0100

    rc.lua update

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -57,8 +57,8 @@ layouts = {
 -- {{{ Tags
 tags = {
   names  = { "bash", "web", "pdf/compile", "vi/tex", "vi/tex", "Git/bash", "IDE/Code", "im", "media" },
-  layout = { layouts[1], layouts[4], layouts[1], layouts[1], layouts[1],
-             layouts[1], layouts[1], layouts[1], layouts[4]
+  layout = { layouts[3], layouts[4], layouts[4], layouts[1], layouts[1],
+             layouts[4], layouts[1], layouts[1], layouts[4]
 }}
 
 for s = 1, scount do
